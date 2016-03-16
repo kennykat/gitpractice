8fa7b4aa29570f9a7ddab0b280fb8ac4355b7d13
Kendra Iraheta <irahetakendra@gmail.com>
2016-03-15 18:27:00 -0700

    added new file and added comments to first file

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-real words
+# this is a git practice file
+
+# git add 'file' >> commits the change
+# git status >> allows you to see what is the status of the repository
+
+# git commit -m >> adding a comment to the code
+# git diff >> tells you line for lien what has been changed
