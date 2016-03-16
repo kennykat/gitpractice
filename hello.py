52a3bbb618c87423b364e4fa232ccc615e42b213
Kendra Iraheta <irahetakendra@gmail.com>
2016-03-15 19:27:12 -0700

    uncommented lines

--- hello.py
+++ hello.py
@@ -1,31 +1,31 @@
-# this is a git practice file
+this is a git practice file
 
-# git add 'file' >> commits the change
-# git status >> allows you to see what is the status of the repository
+git add 'file' >> commits the change
+git status >> allows you to see what is the status of the repository
 
-# git commit -m " " >> adding a comment to the code
-# git diff >> tells you line for line what has been changed
-# you can add the number of the commit log to see specifics
-# (don't need the whole number, can use the first half)
+git commit -m " " >> adding a comment to the code
+git diff >> tells you line for line what has been changed
+you can add the number of the commit log to see specifics
+(don't need the whole number, can use the first half)
 
-# git log >> shows you the time things happened
-# git reset --hard "commit number" >> resets to the last saved point
+git log >> shows you the time things happened
+git reset --hard "commit number" >> resets to the last saved point
 
-# git remote add origin "https origin of github repository"
-# git push >> adding code online
-# git push -u origin master >> pushing to repository for the first time
-# master is the name of the branch
-# -u >> directs your pushes to the repo you tell it,
-# or defaults to the previous repository
-# git push >> sends code to repository
+git remote add origin "https origin of github repository"
+git push >> adding code online
+git push -u origin master >> pushing to repository for the first time
+master is the name of the branch
+-u >> directs your pushes to the repo you tell it,
+or defaults to the previous repository
+git push >> sends code to repository
 
-# git rm -rf "repo name" >> deletes the repo and all it entails
-# git clone "repo https from github" >> clones a repo from github onto your local host
+git rm -rf "repo name" >> deletes the repo and all it entails
+git clone "repo https from github" >> clones a repo from github onto your local host
 
-# git pull takes what has been changed online to your local host
-
-
+git pull takes what has been changed online to your local host
 
 
 
-## terminal python --version >> shows python running on OS
+
+
+# terminal python --version >> shows python running on OS
