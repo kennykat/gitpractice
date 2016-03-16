399ac3dcbea61d7a7418d6a6a481ed8c5e6ec987
Kendra Iraheta <irahetakendra@gmail.com>
2016-03-15 18:51:35 -0700

    new print line

--- hello.py
+++ hello.py
@@ -3,5 +3,17 @@
 # git add 'file' >> commits the change
 # git status >> allows you to see what is the status of the repository
 
-# git commit -m >> adding a comment to the code
-# git diff >> tells you line for lien what has been changed
+# git commit -m " " >> adding a comment to the code
+# git diff >> tells you line for line what has been changed
+# you can add the number of the commit log to see specifics
+# (don't need the whole number, can use the first half)
+
+# git log >> shows you the time things happened
+# git reset --hard "commit number" >> resets to the last saved point
+
+# git remote add origin "https origin of github repository"
+# git push >> adding code online
+# git push -u origin master >> pushing to repository for the first time
+# master is the name of the branch
+# -u >> directs your pushes to the repo you tell it,
+# or defaults to the previous repository
